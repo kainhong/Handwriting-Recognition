dc69e1760a46b2dd332922e785d71a52f7187c4d
Kain <hong.l@yougou.com>
2014-01-26 10:55:59 +0800

    init

---
+++ Handwriting.Recognition/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace 手写识别
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ink_here = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.shibie_0 = new System.Windows.Forms.Button();
+            this.shibie_9 = new System.Windows.Forms.Button();
+            this.shibie_8 = new System.Windows.Forms.Button();
+            this.shibie_7 = new System.Windows.Forms.Button();
+            this.shibie_6 = new System.Windows.Forms.Button();
+            this.shibie_2 = new System.Windows.Forms.Button();
+            this.shibie_3 = new System.Windows.Forms.Button();
+            this.shibie_5 = new System.Windows.Forms.Button();
+            this.shibie_4 = new System.Windows.Forms.Button();
+            this.shibie_1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ink_here)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ink_here
+            // 
+            this.ink_here.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ink_here.Location = new System.Drawing.Point(5, 0);
+            this.ink_here.Name = "ink_here";
+            this.ink_here.Size = new System.Drawing.Size(659, 156);
+            this.ink_here.TabIndex = 0;
+            this.ink_here.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(371, 162);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "识别";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(515, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "清除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "墨水颜色";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // shibie_0
+            // 
+            this.shibie_0.Location = new System.Drawing.Point(5, 161);
+            this.shibie_0.Name = "shibie_0";
+            this.shibie_0.Size = new System.Drawing.Size(163, 23);
+            this.shibie_0.TabIndex = 24;
+            this.shibie_0.Text = "0";
+            this.shibie_0.UseVisualStyleBackColor = true;
+            // 
+            // shibie_9
+            // 
+            this.shibie_9.Location = new System.Drawing.Point(174, 253);
+            this.shibie_9.Name = "shibie_9";
+            this.shibie_9.Size = new System.Drawing.Size(163, 23);
+            this.shibie_9.TabIndex = 23;
+            this.shibie_9.Text = "9";
+            this.shibie_9.UseVisualStyleBackColor = true;
+            // 
+            // shibie_8
+            // 
+            this.shibie_8.Location = new System.Drawing.Point(174, 230);
+            this.shibie_8.Name = "shibie_8";
+            this.shibie_8.Size = new System.Drawing.Size(163, 23);
+            this.shibie_8.TabIndex = 22;
+            this.shibie_8.Text = "8";
+            this.shibie_8.UseVisualStyleBackColor = true;
+            // 
+            // shibie_7
+            // 
+            this.shibie_7.Location = new System.Drawing.Point(174, 207);
+            this.shibie_7.Name = "shibie_7";
+            this.shibie_7.Size = new System.Drawing.Size(163, 23);
+            this.shibie_7.TabIndex = 21;
+            this.shibie_7.Text = "7";
+            this.shibie_7.UseVisualStyleBackColor = true;
+            // 
+            // shibie_6
+            // 
+            this.shibie_6.Location = new System.Drawing.Point(174, 184);
+            this.shibie_6.Name = "shibie_6";
+            this.shibie_6.Size = new System.Drawing.Size(163, 23);
+            this.shibie_6.TabIndex = 20;
+            this.shibie_6.Text = "6";
+            this.shibie_6.UseVisualStyleBackColor = true;
+            // 
+            // shibie_2
+            // 
+            this.shibie_2.Location = new System.Drawing.Point(5, 207);
+            this.shibie_2.Name = "shibie_2";
+            this.shibie_2.Size = new System.Drawing.Size(163, 23);
+            this.shibie_2.TabIndex = 19;
+            this.shibie_2.Text = "2";
+            this.shibie_2.UseVisualStyleBackColor = true;
+            // 
+            // shibie_3
+            // 
+            this.shibie_3.Location = new System.Drawing.Point(5, 230);
+            this.shibie_3.Name = "shibie_3";
+            this.shibie_3.Size = new System.Drawing.Size(163, 23);
+            this.shibie_3.TabIndex = 18;
+            this.shibie_3.Text = "3";
+            this.shibie_3.UseVisualStyleBackColor = true;
+            // 
+            // shibie_5
+            // 
+            this.shibie_5.Location = new System.Drawing.Point(174, 161);
+            this.shibie_5.Name = "shibie_5";
+            this.shibie_5.Size = new System.Drawing.Size(163, 23);
+            this.shibie_5.TabIndex = 17;
+            this.shibie_5.Text = "5";
+            this.shibie_5.UseVisualStyleBackColor = true;
+            // 
+            // shibie_4
+            // 
+            this.shibie_4.Location = new System.Drawing.Point(5, 253);
+            this.shibie_4.Name = "shibie_4";
+            this.shibie_4.Size = new System.Drawing.Size(163, 23);
+            this.shibie_4.TabIndex = 16;
+            this.shibie_4.Text = "4";
+            this.shibie_4.UseVisualStyleBackColor = true;
+            // 
+            // shibie_1
+            // 
+            this.shibie_1.Location = new System.Drawing.Point(5, 184);
+            this.shibie_1.Name = "shibie_1";
+            this.shibie_1.Size = new System.Drawing.Size(163, 23);
+            this.shibie_1.TabIndex = 15;
+            this.shibie_1.Text = "1";
+            this.shibie_1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 278);
+            this.Controls.Add(this.shibie_0);
+            this.Controls.Add(this.shibie_9);
+            this.Controls.Add(this.shibie_8);
+            this.Controls.Add(this.shibie_7);
+            this.Controls.Add(this.shibie_6);
+            this.Controls.Add(this.shibie_2);
+            this.Controls.Add(this.shibie_3);
+            this.Controls.Add(this.shibie_5);
+            this.Controls.Add(this.shibie_4);
+            this.Controls.Add(this.shibie_1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ink_here);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "手写识别";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ink_here)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ink_here;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button shibie_0;
+        private System.Windows.Forms.Button shibie_9;
+        private System.Windows.Forms.Button shibie_8;
+        private System.Windows.Forms.Button shibie_7;
+        private System.Windows.Forms.Button shibie_6;
+        private System.Windows.Forms.Button shibie_2;
+        private System.Windows.Forms.Button shibie_3;
+        private System.Windows.Forms.Button shibie_5;
+        private System.Windows.Forms.Button shibie_4;
+        private System.Windows.Forms.Button shibie_1;
+    }
+}
+
---
+++ Handwriting.Recognition/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Ink;
+
+namespace 手写识别
+{
+    public partial class Form1 : Form
+    {
+        InkCollector ic;
+        RecognizerContext rct;
+       // Recognizer rc;
+        string FullCACText;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.rct.RecognitionWithAlternates += new RecognizerContextRecognitionWithAlternatesEventHandler(rct_RecognitionWithAlternates);
+
+            ic = new InkCollector(ink_here.Handle);
+            this.ic.Stroke += new InkCollectorStrokeEventHandler(ic_Stroke);
+
+            ic.Enabled = true;
+            ink_();
+
+         //   this.ic.Stroke += new InkCollectorStrokeEventHandler(ic_Stroke);
+          this.rct.RecognitionWithAlternates += new RecognizerContextRecognitionWithAlternatesEventHandler(rct_RecognitionWithAlternates);
+
+           rct.Strokes = ic.Ink.Strokes;
+           
+          
+
+        }
+
+        void rct_RecognitionWithAlternates(object sender, RecognizerContextRecognitionWithAlternatesEventArgs e)
+        {
+           
+             string ResultString="";
+             RecognitionAlternates alts;
+         
+            if (e.RecognitionStatus == RecognitionStatus.NoError)            
+            {
+                alts = e.Result.GetAlternatesFromSelection();
+                
+              foreach(RecognitionAlternate alt in alts)
+              {
+                  ResultString = ResultString + alt.ToString()+" ";                  
+              }        
+            }
+            FullCACText = ResultString.Trim();
+            Control.CheckForIllegalCrossThreadCalls=false;            
+            textBox1.Text = FullCACText;
+            返回识别字符处理(FullCACText);
+            Control.CheckForIllegalCrossThreadCalls =true;            
+
+        }
+
+        private void 返回识别字符处理(string 字符)
+        {
+            string[] str_temp = 字符.Split(' ');
+            string str_temp1 = "shibie_";
+            string str_temp2 = "";
+            if (str_temp.Length > 0)
+            {
+                for (int i = 0; i < str_temp.Length; i++)
+                {
+                    str_temp2 = str_temp1 + i.ToString();
+                    Control[] con_temp = Controls.Find(str_temp2, true);
+                    if (con_temp.Length > 0)
+                    {
+                        ((Button)(con_temp[0])).Text = str_temp[i];
+                    }
+                }
+            }
+        }
+
+        void ic_Stroke(object sender, InkCollectorStrokeEventArgs e)
+        {
+            rct.StopBackgroundRecognition();
+            rct.Strokes.Add(e.Stroke);
+            rct.CharacterAutoCompletion = RecognizerCharacterAutoCompletionMode.Full;
+            rct.BackgroundRecognizeWithAlternates(0);
+        }
+
+        private void ink_()
+        {
+            Recognizers recos = new Recognizers();           
+            Recognizer chineseReco  = recos.GetDefaultRecognizer();
+
+            rct = chineseReco.CreateRecognizerContext();
+        }
+        private void ic_Stroke()
+        {
+ 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.FullOpen = true;
+            colorDialog1.ShowDialog();
+            ic.DefaultDrawingAttributes.Color = colorDialog1.Color;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!ic.CollectingInk)
+            {
+                Strokes strokesToDelete = ic.Ink.Strokes;                
+                rct.StopBackgroundRecognition();
+                ic.Ink.DeleteStrokes(strokesToDelete);
+                rct.Strokes = ic.Ink.Strokes;
+                ic.Ink.DeleteStrokes();//清除手写区域笔画;
+                ink_here.Refresh();//刷新手写区域
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+          
+            
+            textBox1.SelectedText = ic.Ink.Strokes.ToString();
+          /*  if (0 == rc.Count)
+            {
+                MessageBox.Show("There are no handwriting recognizers installed.  You need to have at least one in order to recognize ink.");
+            }
+            else
+            {
+
+                // Note that the Strokes' ToString() method is a 
+                // shortcut  for retrieving the best match using the  
+                // default recognizer.  The same result can also be 
+                // obtained using the RecognizerContext.  For an 
+                // example, uncomment the following lines of code:
+                // 
+                 RecognizerContext myRecoContext = new RecognizerContext();
+                 myRecoContext.CharacterAutoCompletion = RecognizerCharacterAutoCompletionMode.Full;
+                 RecognitionStatus status;
+                RecognitionResult recoResult;
+                //
+                 myRecoContext.Strokes = ic.Ink.Strokes;
+                recoResult = myRecoContext.Recognize(out status);
+                textBox1.SelectedText = recoResult.TopString;
+                //
+                
+               // textBox1.SelectedText = ic.Ink.Strokes.ToString();
+            }*/
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
